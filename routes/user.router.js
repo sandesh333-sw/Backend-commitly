157235ea35cccda8e6c9816a141beb5fddeb26da
sandesh333-sw <sandeshpulami333@gmail.com>
2025-08-15 12:32:39 +0100

    Fix circular dependency in router files

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const userController = require("../controllers/userController");
-const mainRouter = require("./main.router");
 
 const userRouter = express.Router();
 
